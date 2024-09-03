4980bac7d24200871d0623f4d33ed3ef25279da5
Srinivas <kolipakasrinivas456@gmail.com>
2024-09-03 12:10:30 +0530

    changed the paths in css files

--- src/app/dashboard/products/[id]/page.jsx
+++ src/app/dashboard/products/[id]/page.jsx
@@ -1,4 +1,4 @@
-import styles from "@/app/ui/dashboard/products/singleProduct/singleProduct.module.cssr";
+import styles from "@/app/ui/dashboard/products/singleProduct/singleProduct.module.css";
 import Image from "next/image";
 import { fetchProduct } from "@/app/lib/data";
 import { updateProduct } from "@/app/lib/actions";
