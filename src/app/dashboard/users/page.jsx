e245190dda41b0e2988252b205e84d66da6848eb
Srinivas <kolipakasrinivas456@gmail.com>
2024-09-03 12:28:08 +0530

    updated the user css file

--- src/app/dashboard/users/page.jsx
+++ src/app/dashboard/users/page.jsx
@@ -1,4 +1,4 @@
-import styles from "@/app/ui/dashboard/dashboard.module.css";
+import styles from "@/app/ui/dashboard/users/search.module.css";
 import Search from "@/app/ui/dashboard/search/Search";
 import Image from "next/image";
 import Link from "next/link";
